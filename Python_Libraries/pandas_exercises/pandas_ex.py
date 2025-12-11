3fc40203267932ee17258c972db1f6e45ba5c1d7
k0che1 <123996932+k0che1@users.noreply.github.com>
2025-12-11 10:39:04 +0300

    Add files via upload

---
+++ Python_Libraries/pandas_exercises/pandas_ex.py
@@ -0,0 +1,300 @@
+df = pd.DataFrame({'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+                   'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+                   'name': ['Murzik', 'Pushok', 'Kaa', 'Bobik', 'Strelka', 'Vaska', 'Kaa2', 'Murka', 'Graf', 'Muhtar'],
+                   'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                   'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']},
+                   index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+'''
+Создайте Dataframe из словаря data. В качестве подписей строк используйте список labels.
+'''
+import pandas as pd
+#1
+df = pd.DataFrame(data=data,index=labels)
+#2
+df = pd.DataFrame(data,labels)
+
+
+'''
+Переменная df содержит DataFrame.Переменная col содержит имя колонки.Переменная row содержит имя ИЛИ индекс строки.Выведите на печать содержимое ячейки.
+'''
+#1
+print(df.loc[row,col])
+#2
+print(df.at[row, col])
+
+
+'''
+Переменная df содержит DataFrame.Выведите на печать:число непустых (не null) значений в колонке 'age',75% квантиль для значений в колонке 'age'
+'''
+#1
+buffer = io.StringIO() 
+df.info(buf=buffer)
+a= buffer.getvalue()
+L=[]
+a.splitlines()
+for line in a.splitlines():
+  L.append(line.split())
+print(float(L[6][2]))
+print(df["age"].quantile(0.75))
+#2
+print(df.describe()['age']['count'])
+print(df.describe()['age']['75%'])
+
+
+'''
+Переменная df содержит DataFrame. Выведите на печать 3 первых строки.
+'''
+#1
+print(df[:3])
+#2
+print(df.iloc[:3])
+#3
+print(df.head(3))
+
+
+'''
+Переменная df содержит DataFrame. Выведите на печать строки с индексами 0, 2, 3.
+'''
+print(df.iloc[[0,2,3]])
+
+
+'''
+Переменная df содержит DataFrame. Выведите на печать только столбцы 'name' и 'age'
+'''
+#1
+print(df.loc[:,['name','age']])
+#2
+print(df[['name', 'age']])
+
+
+'''
+Переменная df содержит DataFrame.Выведите на печать только столбцы 'name' и 'age' И строки 0, 2, 3
+'''
+print(df.iloc[[0,2,3]][['name', 'age']])
+
+
+'''
+Переменная df содержит DataFrame. Выведите на печать только данные, относящиеся к записям, возраст которых больше, указанного в переменной critical_age.
+'''
+print(df[df.age>critical_age])
+
+
+'''
+Переменная df содержит DataFrame.Выведите на печать только данные, в которых не заполнен возраст (в графе 'age' стоит null).
+'''
+print(df[df.age.isna()])
+
+
+'''
+Переменная df содержит DataFrame. Переменная filter_names содержит список имён столбцов по которым происходит фильтрация. Переменная filter_values содержит список 
+из 2 значений по которым происходит фильтрация. 1 значение проверяется на равенство. 2 значение должно быть меньше указанного в фильтре. 
+Выведите только те строки датафрейма, которые удовлетворяют условиям обоих фильтров.
+'''
+print(df[(df[filter_names[0]]==filter_values[0]) & (df[filter_names[1]]<filter_values[1])])
+
+
+'''
+Переменная df - DataFrame.Переменная age_between содержит 2 числа. Выведите только те строки датафрейма, возраст в которых находится в интервале между числами age_between (включая границы).
+'''
+#1
+print(df[(df['age']>=age_between[0]) & (df['age']<=age_between[1])])
+#2
+print(df[df['age'].between(*age_between)])
+
+
+'''
+Переменная df содержит DataFrame. Переменная index содержит строку ИЛИ число (индекс).Увеличьте значение возраста в строке индексом равным index на 1.
+'''
+#1
+df.loc[index,'age']+=1
+#2
+df['age'][index] += 1
+
+
+'''
+Переменная df содержит DataFrame. Выведите на печать сумму всех возрастов в df.
+'''
+#1
+print(df['age'].sum())
+#2
+print(np.sum(df.age))
+
+
+'''
+Переменная df содержит DataFrame.Выведите на печать для каждого столбца, содержащего числа его имя и сумму через двоеточие.
+'''
+#1
+for x in df.describe():
+    print(x + ":" + str(df[x].sum()))
+
+#2
+c=df.sum(numeric_only=True).index.values.tolist()
+a=df.sum(numeric_only=True)
+for i in range(len(c)):
+  print(f'{c[i]}:{a[i]}')
+
+
+'''
+df - DataFrame.Переменная group_by содержит имя колонки по которой производится группировка. Найдите средние значение возраста по всем записям, сгруппированным по значению 
+в колонке group_by и выведите на печать. Например, если group_by='animal', то для первого Dataframe надо получить таблицу со средними для каждого вида животных: кошек, собак, змей.
+'''
+print(df.groupby([group_by])['age'].mean())
+
+
+'''
+Переменная df содержит DataFrame. new_index - индекс для строки, которую надо добавить, new_data - значения для строки, которую надо добавить, del_index - индекс строки, которую надо удалить.
+Добавьте новую строку (с индексом new_index и значениями new_data) и удалите одну из старых (del_index)
+'''
+df.loc[new_index]=new_data
+df.drop(del_index, inplace=True)
+# df = df.drop(del_index)
+
+
+'''
+Переменная df содержит DataFrame.Переменная group_by содержит имя колонки по которой производится группировка. Найдите количество записей каждого типа, сгруппированным 
+по значению в колонке group_by.Например, если group_by='animal', то для первого Dataframe надо получить таблицу с количеством каждого вида животных: кошек, собак, змей.
+'''
+print(df[group_by].value_counts())
+
+
+'''
+Переменная df содержит DataFrame.Переменная sort_by содержит список из 2 строк - имён столбцов по которым необходимо провести сортировку.
+Отсортируйте сперва по уменьшению 1 поля из списка sort_by, а при равенстве значений по увеличению 2.
+'''
+#1
+df.sort_values(*[sort_by],ascending=[False,True], inplace=True)
+print(df)
+#2
+print(df.sort_values(sort_by, ascending=(False, True)))
+
+
+'''
+Переменная df содержит DataFrame. Переменная column содержит имя колонки, содержащей строковые значения "yes", "no", либо числовые 1 или 0.
+Замените в column: "yes" и 1 на True, "no" и 0 на False
+'''
+#1
+z = {'yes': True, 'no': False, 0: False, 1: True
+df[column] =df[column].map(z)
+#2
+df[column] = df[column].isin([1, 'yes'])
+
+
+'''
+Переменная df содержит DataFrame. Переменная column содержит имя колонки, значения в которой надо модифицировать. Переменные old_value и new_value содержат старое 
+и новое значения, соответственно.Замените все старые значения на новые в соответсвующей колонке.
+'''
+df[column]=df[column].replace(old_value,new_value)
+
+
+'''
+В файле torg.csv представлена выгрузка со склада интернет-магазина. Товаров какого цвета больше всего на складе?
+'''
+df = pd.read_csv('torg.csv', sep=';')
+df_group = df[['IP_PROP30', 'CP_QUANTITY']].groupby('IP_PROP30')
+print(df_group.sum().idxmax())
+
+
+'''
+В файле torg.csv представлена выгрузка со склада интернет-магазина. Сгруппируйте размеры представленных товаров по суммарному количеству единиц товара на складе.
+'''
+df_group = df[['IP_PROP32', 'CP_QUANTITY']].groupby('IP_PROP32')
+print(df_group.sum().sort_values(by='CP_QUANTITY'))
+
+
+'''
+В этом задании представлена выгрузка складских остатков. Найдите суммарную стоимость всех розовых (pink) вещей большого размера (XL).
+'''
+df['cost'] = df.CP_QUANTITY * df.CR_PRICE_1_USD
+df=df[(df['IP_PROP30']=='pink')&(df['IP_PROP32']=='XL')].cost.sum()
+
+
+
+'''
+В файле StudentsPerformance.csv - выгрузкf об абитуриентах.Каково максимальное медианное значение оценки по чтению у групп, разделенных по признаку "пол абитуриента" и "национальность" ?
+'''
+df=df.groupby(['gender','race/ethnicity'])['reading score'].median().max()
+
+
+'''
+Продолжаем работать с датасетом StudentsPerformance.csv. Какое среднее значение оценок по всем предметам у мальчиков из такой же группы с уровнем образования,
+как у родителей девочек, получивших максимальную среднюю оценку по всем предметам?
+'''
+df = pd.read_csv('StudentsPerformance.csv')
+#группируемся по колонкам "пол абитуриента" и "уровень образования родителей"
+#считаем среднее по предметам
+a=df.groupby(['gender','parental level of education']).mean()
+print(a,'\n')
+b=a.loc['female']
+print(b,'\n')
+#для девочек ищем максимум среднего 
+#получаем название группы "уровень образования родителей"
+c=b.idxmax()
+print(c[0],'\n')
+#для мальчиков получаем среднюю оценку по этой группе.
+e=a.loc[(  'male',c[0])].mean()
+print(e)
+
+
+'''
+Для этого задания используется случайно генерируемая выборка из 100 записей об использовании велодорожек по датам. В выгрузке присутствуют данные только о 7 дорожках (в остальных пустые значения).
+Посчитайте среднюю посещаемость каждой велодорожки отдельно по дням недели. Какой день недели (в среднем) наиболее посещаем?
+'''
+df = pd.read_csv('dataset_days-of-week.txt',sep=',')
+#создаем колонку с днем недели
+df['Date'] = pd.to_datetime(df['Date'])
+df['day_of_week'] = df['Date'].dt.dayofweek
+#сортируем посещяемость каждой велодорожки отдельно по дням недели, затем по всем дорожкам
+c=df.groupby('day_of_week').mean().mean(axis=1)
+#выводим самую посещаемую
+print(c.idxmax())
+
+
+'''
+В файле football_players.csv представлен фрагмент выгрузки датасета о футболистах.Посчитайте среднюю и медианную зарплату "Wage" футболистов из разных клубов "Club".
+В скольких клубах средняя и медианная зарплаты совпадают?
+'''
+#1
+df=pd.read_csv('football_players.csv')
+a=df.groupby('Club')['Wage'].describe(include='all')['mean']
+b=df.groupby('Club')['Wage'].describe(include='all')['50%']
+print(list(a-b).count(0))
+#2
+df=pd.read_csv('football_players.csv')
+a=df.groupby('Club')['Wage'].describe()[['mean','50%']]
+c=a[a['mean']==a['50%']]
+print(len(c))
+
+
+'''
+Посчитайте количество организаций (CompanyID), у которых суммарный объем файлов (FileSize) хотя бы одного проекта (ProjectID) превышает средний объем файлов по всем проектам.
+'''
+df = pd.read_csv('dataset_file_storage.csv',sep=';')
+#Находим суммарный объем файлов для каждого проекта
+a=df.groupby('ProjectID')['FileSize'].sum()
+# Находим среднее суммарных объемов из предыдущей таблицы
+sumMean=a.mean()
+#Делаем группировку по компаниb и проекту для нахождения суммарного размера файла для каждого проекта компани
+b=df.groupby(['CompanyID','ProjectID'])['FileSize'].sum()
+#Применяем фильтр предыдущей таблице
+c=b[b>sumMean]
+Z=c.index.values
+X=[Z[i][0] for i in range(len(Z))]
+print(len(set(X)))
+
+
+'''
+На какую сумму купили и оплатили носков любых видов?
+'''
+prod = pd.read_csv('Products.csv',sep=';')
+ord=pd.read_csv('orders.csv',sep=';')
+#объединяем таблицы                
+df=pd.merge(ord, prod, how ='left', left_on=('ID товара'), right_on=('Product_ID'))
+#сортируем по статусу и "Носкам"
+df=df[(df['Оплачен'] == 'Да') & (df['Name'].str.contains('Носки'))]
+#считаем стоимость каждого заказа
+df['cost']=df['Количество']*df['Price']
+#выводим сумму
+#1
+print(df['cost'].sum())
+#2
+print(sum(df["Количество"] * df["Price"]))
